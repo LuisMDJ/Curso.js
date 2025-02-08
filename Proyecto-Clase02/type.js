a52ffcf52ea26a9ba871cca74a5b5c51965d7432
LuisMDJ <monroyluis195@gmail.com>
2025-02-08 16:44:25 -0600

    type.js

--- Proyecto-Clase02/type.js
+++ Proyecto-Clase02/type.js
@@ -129,9 +129,9 @@ let nota = Math.floor(Math.random() * 101);
 console.log(nota);
 if (nota >= 90) { 
     console.log("Excelente");
-} else if (nota >= 75 && nota < 89) {
+} else if (nota >= 75 && nota <= 89) {
     console.log("Bien");
-} else if (nota >= 60 && nota < 74) {
+} else if (nota >= 60 && nota <= 74) {
     console.log("Suficiente");
 } else {
     console.log("No aprueba");
