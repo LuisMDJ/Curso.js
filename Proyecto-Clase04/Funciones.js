2da21350f7f6c06c4d6795bbdea35166b16588c2
LuisMDJ <monroyluis195@gmail.com>
2025-02-22 12:07:58 -0600

    C4JS

--- Proyecto-Clase04/Funciones.js
+++ Proyecto-Clase04/Funciones.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //* Proyecto - Clase 04
 //Declarar el array
 let librosLeidos = [];
@@ -31,37 +30,3 @@ AñadirLibro("El diario de Ana Frank");
 AñadirLibro("IT");
 
 MostrarLibrosLeidos();
-=======
-//* Proyecto - Clase 04
-//Declarar el array
-let librosLeidos = [];
-//Funcion para añadir libros
-function AñadirLibro(titulo) {
-    if (titulo && typeof titulo === "string") {
-        librosLeidos.push(titulo);
-        console.log(`"${titulo}" ha sido añadido a la lista de libros leidos.`);
-    } else {
-        console.log("Ingrese un título válido");
-    }
-}
-//Funcion para mostrar libros leidos
-function MostrarLibrosLeidos() {
-    if (librosLeidos.length === 0) {
-        console.log("No hay libros leidos");
-    } else {
-        console.log("Libros leidos:");
-        librosLeidos.forEach((libro, index) => {
-            console.log(`${index + 1}. ${libro}`);
-        })
-        }
- }
-// Ejemplos de uso
-AñadirLibro("Insoportable levedad del ser");
-AñadirLibro("El principito");
-AñadirLibro("Cien años de soledad");
-AñadirLibro("1984");
-AñadirLibro("El diario de Ana Frank");
-AñadirLibro("IT");
-
-MostrarLibrosLeidos();
->>>>>>> 7b89eaa74cbc062ba0a2e7d0ca0c3401310918e3
